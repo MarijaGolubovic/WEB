16c588809d826a1fd82b6f30855b24b654c55184
Marija Golubovic <marijagolubovic16@gmail.com>
2022-07-10 15:49:14 +0200

    fix merging conflicts

--- WebShopREST/WebContent/components/Kupac/kupacZaglavlje.js
+++ WebShopREST/WebContent/components/Kupac/kupacZaglavlje.js
@@ -6,11 +6,8 @@ Vue.component("kupac-zaglavlje",{
                 <li><router-link to="/zakazivanje" class="link" exact>Zakazivanje Treninga</router-link></li>
                 <li><router-link to="/zakazani" class="link" exact>Zakazani Treninzi</router-link></li>
                 <li><router-link to="/clanarine" class="link" exact>Clanarine</router-link></li>
-<<<<<<< HEAD
                 <li><router-link to="/komentari" class="link" exact>Ostavi komentar</router-link></li>
-=======
  				<li><router-link to="/profil" class="link" exact>Profil</router-link></li>
->>>>>>> cd21bd3bac996a6759a429fb590ce777ddbbadb0
             </ul>
         </div>
 
